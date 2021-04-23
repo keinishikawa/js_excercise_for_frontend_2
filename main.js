04f544aacdfdc7bc4ac0fd46f089235dd4503251
keinishikawa <keinishikawa4@gmail.com>
2021-04-23 12:23:18 +0900

    done

--- main.js
+++ main.js
@@ -3,7 +3,8 @@
  *   - textContentを使う
  *     - 「JSで変更した後のタイトル」というテキストをセットする
  */
-
+titleDom =　document.getElementById("title");
+titleDom.textContent = "JSで変更した後のタイトル";
 
 /**
  * 課題2: p要素でid属性値が `description` のテキスト内容を変更する
@@ -11,7 +12,7 @@
  *     - 「JSで変更した後の説明文」というテキストをセットする
  *   - strong要素を使って「説明文」を強調する
  */
-
+document.getElementById("description").innerHTML = "JSで変更した後の<strong>説明文</strong>";
 
  /**
   * 課題3: img要素でid属性値が 'profile' のsrc属性値を変更する
@@ -19,11 +20,11 @@
   *     - 自分で用意する
   *     - 「https://www.pakutaso.com/shared/img/thumb/TOMnemuiDSC01757_TP_V1.jpg」を使う
   */
-
+document.getElementById("profile").src = "https://www.pakutaso.com/shared/img/thumb/TOMnemuiDSC01757_TP_V1.jpg";
 
 /**
  * 課題4: 課題1で作成したh1要素のDOMにクラス属性値をセットする
  *   - セットするクラス属性値はstyle.cssの中身を参考にする
  *     - 背景色がh1要素の背景色が黄色になればOKです
  */
-
+titleDom.className = "bg-yellow";
